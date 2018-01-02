dda82e412a1718af2e5d471d9f98908609b5a3f7
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2018-01-02 04:43:56 -0500

    Rename to Oro

--- conf.lua
+++ conf.lua
@@ -1,7 +1,7 @@
 function love.conf(t)
   t.version = "0.10.1"
 
-  t.window.title = "Ouroboros"
+  t.window.title = "Oro"
   t.window.width = 640
   t.window.height = 576
   t.window.borderless = true
